def93b9c4e9fbcf56dafd1998c59f35658cab672
Indra Parameswaran <indrakumari.param@hcl.com>
2020-12-30 08:01:56 -0500

    ExceptionHandling file initial commit

---
+++ src/main/java/com/hcl/ExceptionHandling/App.java
@@ -0,0 +1,57 @@
+package com.hcl.ExceptionHandling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	aExUnHandled();
+		//aExHandled();
+    	//aExThrow();
+    	
+		System.out.println("End");
+    
+      
+    }
+    
+    public static void aExThrow() throws Exception {
+		
+  		//int result = 0;
+  		//result = 5/0;
+  		
+  		int result = 2;
+  		result = 10/2;
+  		//throw new Exception("Throw Exception : ");
+  		System.out.println(" Result is : " + result);
+  	
+
+  	}
+    
+    public static void aExUnHandled() {
+		
+		//int result = 0;
+		//result = 5/0;
+		
+		int result = 2;
+		result = 10/2;
+		System.out.println(" Result is : " + result);
+	
+
+	}
+
+	public static void aExHandled() {
+		try {
+		int result = 0;
+		result = 5/0;
+		} catch(ArithmeticException ex) {
+			System.out.println(ex.getMessage());
+		} finally {
+			System.out.println("In the Finally block");
+		}
+	}
+	
+}
+
